aa0eedce22c3e31ed3d40826f2faa82d7770923d
xinzhao <xinzhao@deepglint.com>
2017-02-16 16:42:02 +0800

    write evaluate result to file

--- cpp/src/evaluate_main.cxx
+++ cpp/src/evaluate_main.cxx
@@ -18,14 +18,14 @@ void ParseTag(const string& fn, vector<Record> &records) {
     ifstream ifs(fn.c_str(), ios::in);
     string line;
     while( getline(ifs, line) ) {
-        cout<<line<<endl;
+        //cout<<line<<endl;
         Record record;
         stringstream ss;
         ss<<line;
         ss>>record.first;
         ss>>record.second; 
         records.push_back(record);
-        printf("first = %s, second = %f\n", record.first.c_str(), record.second);
+        //printf("first = %s, second = %f\n", record.first.c_str(), record.second);
     } 
     ifs.close();
 } 
@@ -50,56 +50,57 @@ int main(int argc, char* argv[]) {
     int batch_num = records.size()/batch_size;
     float avg_time = 0;
 
-    DEBUG_PRINT();
+    ofstream ofs(result_loc.c_str(), ios::out);
+
     CaffeClassifier *classifier_ = new CaffeClassifier(argv[1], argv[2]);
-    DEBUG_PRINT();
     // batch operation of images
     for ( int batch_count = 0; batch_count<batch_num; batch_count++) {
         vector<Mat> batch_inputs;
-        vector<string> batch_img_fns;
+        vector<Record> batch_records;
         vector< vector<float> > batch_preds;
         for ( int k=batch_count*batch_size; k<(batch_count+1)*batch_size; k++ ) {
             Mat img = imread(records[k].first);
             batch_inputs.push_back(img);
-            batch_img_fns.push_back(records[k].first);
+            batch_records.push_back(records[k]);
         }
 
         unsigned long long begin = GetCurrentMicroSecond();
-        DEBUG_PRINT();
         classifier_->Predict(batch_inputs, batch_preds);
-        DEBUG_PRINT();
         unsigned long long end = GetCurrentMicroSecond();
         printf("batch time %lldms\n", end - begin);
         avg_time += end-begin;
 
         for (int i=0; i<batch_size; i++) {
-            cout<<batch_img_fns[i]<<"\t";
+            ofs<<batch_records[i].first<<"\t";
             for (vector< vector<float> >::size_type j=0; j<batch_preds[i].size(); j++) {
-                cout<<batch_preds[i][j]<<"\t";
+                ofs<<batch_preds[i][j]<<"\t";
             }
-            cout<<endl;
+            ofs<<batch_records[i].second;
+            ofs<<endl;
         }
 
     }
 
     // operation of tail of images 
     vector<Mat> batch_inputs;
-    vector<string> batch_img_fns;
+    vector<Record> batch_records;
     vector< vector<float> > batch_preds;
     for ( vector<string>::size_type i = batch_size*batch_num; i<records.size(); i++) {
         Mat img = imread(records[i].first);
-        batch_img_fns.push_back(records[i].first);
+        batch_records.push_back(records[i]);
         batch_inputs.push_back(img);
     }
     classifier_->Predict(batch_inputs, batch_preds);
-    for (vector<string>::size_type i=0; i<batch_img_fns.size(); i++) {
-        cout<<batch_img_fns[i]<<"\t";
+    for (vector<string>::size_type i=0; i<batch_records.size(); i++) {
+        ofs<<batch_records[i].first<<"\t";
         for (vector< vector<float> >::size_type j=0; j<batch_preds[i].size(); j++) {
-            cout<<batch_preds[i][j]<<"\t";
+            ofs<<batch_preds[i][j]<<"\t";
         }
-        cout<<endl;
+        ofs<<batch_records[i].second;
+        ofs<<endl;
     }
-
+    
+    ofs.close();
     delete classifier_;
 
     return 0;
